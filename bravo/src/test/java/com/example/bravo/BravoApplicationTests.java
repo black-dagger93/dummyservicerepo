754d4a69dc466293ad4895a749d6423463476c08
Chew <yun_leng.chew@mercedes-benz.com>
2025-04-06 09:11:14 +0800

    init Spring Boot apps

---
+++ bravo/src/test/java/com/example/bravo/BravoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BravoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
